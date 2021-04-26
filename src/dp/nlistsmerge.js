294f765f301d70d9001c5f99ffe81016e0f83c89
Wan <wanwebdev@gmail.com>
2021-04-26 00:32:40 -0400

    CtCI -- Merge N linked lists
    Merge an array of N sorted linked lists into a single sorted list, dynprog

---
+++ src/dp/nlistsmerge.js
@@ -0,0 +1,25 @@
+/* === Merge N linked lists ===
+ *
+ * You are given an array of N linked-lists lists, each linked-list is sorted
+ * in ascending order. Merge all the linked-lists into one sorted linked-list
+ * and return it.
+ *
+ * Runtimes:
+ *  Multiple recursion merge: O(nm)
+*/
+
+import { mergeListRecursion } from './listmerge';
+
+/* eslint-disable import/prefer-default-export, no-param-reassign */
+export default mergeNListsRecursion = (A) => {
+  if (!A || !A.length) return null;
+
+  while (A.length > 1) {
+    const listA = A.shift();
+    const listB = A.shift();
+
+    A.push(mergeListRecursion(listA, listB));
+  }
+
+  return A[0];
+};
